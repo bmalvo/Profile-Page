7c2d0a38e894b04dfd902a07157819b56ecc4286
bmalvo <boydmalvo@gmail.com>
2024-01-28 20:00:46 +0100

    add hamburger menu on small devices

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-var i = 0;
-var txt = "Welcome to my portfolio page! I'm Patryk Skonieczny, and I'm constantly working on improving my programming skills in HTML, CSS, JavaScript, and Python. Feel free to scroll through the page to learn more about my work and projects. I would greatly appreciate any suggestions or feedback you may have. You can also connect with me on LinkedIn and GitHub. The links to these platforms are represented by icons on the left. Thank you!";
-var speed = 20;
+let i = 0;
+const txt = "Welcome to my portfolio page! I'm Patryk Skonieczny, and I'm constantly working on improving my programming skills in HTML, CSS, JavaScript, and Python. Feel free to scroll through the page to learn more about my work and projects. I would greatly appreciate any suggestions or feedback you may have. You can also connect with me on LinkedIn and GitHub. The links to these platforms are represented by icons on the left. Thank you!";
+const speed = 20;
+
+const hamburgerButton = document.querySelector('.hamburger');
+const leftBar = document.querySelector('.left-bar');
+
+hamburgerButton.addEventListener('click', () => {
+  leftBar.classList.toggle('open');
+  hamburgerButton.classList.toggle('open');
+})
 
 
 function typeWriter() {
